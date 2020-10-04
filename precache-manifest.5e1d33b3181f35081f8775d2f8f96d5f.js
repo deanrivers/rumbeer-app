f9f43405a8952bf696cde3832a25e84225fee444
deanrivers <deanriversdeveloper@gmail.com>
2020-10-04 18:09:55 -0400

    Updates

---
+++ precache-manifest.5e1d33b3181f35081f8775d2f8f96d5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991dd69c8ab932a5869acda4ae87dc5b",
+    "url": "/rumbeer-app/index.html"
+  },
+  {
+    "revision": "04b295a39f55eae949ce",
+    "url": "/rumbeer-app/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "a7402b2aa3009d5244e3",
+    "url": "/rumbeer-app/static/css/main.c8a9e523.chunk.css"
+  },
+  {
+    "revision": "04b295a39f55eae949ce",
+    "url": "/rumbeer-app/static/js/2.7e280e76.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/rumbeer-app/static/js/2.7e280e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7402b2aa3009d5244e3",
+    "url": "/rumbeer-app/static/js/main.486b627a.chunk.js"
+  },
+  {
+    "revision": "0c860aa447ea0573ef8a",
+    "url": "/rumbeer-app/static/js/runtime-main.7726a0ab.js"
+  }
+]);
