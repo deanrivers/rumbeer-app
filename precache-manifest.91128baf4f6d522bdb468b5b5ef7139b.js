93efa97a810ae23c326667613b435891ada9d9ba
deanrivers <deanriversdeveloper@gmail.com>
2020-10-05 17:46:48 -0400

    Updates

---
+++ precache-manifest.91128baf4f6d522bdb468b5b5ef7139b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41ca5d51b2e9703eed82e67216e394c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "726202db65df495cc456",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "b218b3cacf80b5d51f45",
+    "url": "/static/css/main.32c5ecbe.chunk.css"
+  },
+  {
+    "revision": "726202db65df495cc456",
+    "url": "/static/js/2.35d3253d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.35d3253d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b218b3cacf80b5d51f45",
+    "url": "/static/js/main.ebf518d6.chunk.js"
+  },
+  {
+    "revision": "af0a9c728d890cf2d3c3",
+    "url": "/static/js/runtime-main.9efd9aba.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rumbeer-app/precache-manifest.5e1d33b3181f35081f8775d2f8f96d5f.js"
+  "/precache-manifest.91128baf4f6d522bdb468b5b5ef7139b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rumbeer-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
