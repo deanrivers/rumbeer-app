862b38f2e87c3e0ed9dd7cc2f661f903d36dc6bd
Jose Caicedo <jose@joses-mbp.myfiosgateway.com>
2020-10-06 19:55:16 -0400

    added firebase connection

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,39 @@
-from flask import (Flask, render_template)
+import firebase_admin
+import pyrebase
+import json
+from flask import (Flask, render_template, request)
+from firebase_admin import credentials, auth
+
 
 app = Flask("__main__", 
     static_url_path='', 
     static_folder='../frontend/build', 
     template_folder='../frontend/build')
 
+
+cred = credentials.Certificate('rumbeer-firebase-creds.json')
+firebase = firebase_admin.initialize_app(cred)
+pb = pyrebase.initialize_app(json.load(open('fbConfig.json')))
+
 @app.route("/")
 def serve_react_build():
     return render_template("index.html", flask_token="Hello World")
+
+#Api route to sign up a new user
+@app.route('/api/signup', methods=['POST'])
+def signup():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or password is None:
+        return {'message': 'Error missing email or password'},400
+    try:
+        user = auth.create_user(
+               email=email,
+               password=password
+        )
+        return {'message': f'Successfully created user {user.uid}'},200
+    except:
+        return {'message': 'Error creating user'},400
 
 @app.route("/api/updateStats")
 def update_stats():
