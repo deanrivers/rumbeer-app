0aeea3ec8d90bf01606b81df31d2f54c84f94693
jrc <jrc.dev3@gmail.com>
2020-10-06 22:00:28 -0400

    added login and token endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,38 @@
 import firebase_admin
 import pyrebase
 import json
+from functools import wraps
 from flask import (Flask, render_template, request)
 from firebase_admin import credentials, auth
-
 
 app = Flask(__name__, 
     static_url_path='', 
     static_folder='../frontend/build', 
     template_folder='../frontend/build')
 
-
 cred = credentials.Certificate('rumbeer-firebase-creds.json')
 firebase = firebase_admin.initialize_app(cred)
 pb = pyrebase.initialize_app(json.load(open('fbConfig.json')))
+
+def check_token(f):
+    @wraps(f)
+    def wrap(*args,**kwargs):
+        if not request.headers.get('authorization'):
+            return {'message': 'No token provided'},400
+        try:
+            user = auth.verify_id_token(request.headers['Authorization'])
+            print(user)
+            request.user = user
+        except:
+            return {'message':'Invalid token provided.'},400
+        return f(*args, **kwargs)
+    return wrap
 
 @app.route("/")
 def serve_react_build():
     return render_template("index.html")
 
-#Api route to sign up a new user
+#api route to sign up a new user
 @app.route("/api/signup", methods=["POST"])
 def signup():
     data = request.get_json()
@@ -36,12 +49,26 @@ def signup():
     except:
         return {'message': 'Error creating user'},400
 
-@app.route("/api/login", methods=["POST"])
+#api route to get token for authorization
+@app.route("/api/token", methods=["POST"])
 def login():
     data = request.get_json()
     email = data['email']
     password = data['password']
 
+    if email is None or password is None:
+        return {'message': 'Error missing email or password'},400
+    try:
+        user = pb.auth().sign_in_with_email_and_password(email, password)
+        jwt = user['idToken']
+        return {'token': jwt}, 200
+    except:
+        return {'message': 'There was an error logging in'},400
+
+@app.route('/api/userinfo')
+@check_token
+def userinfo():
+    return {"data": request.user}, 200
 
 @app.route("/api/updateStats")
 def update_stats():
