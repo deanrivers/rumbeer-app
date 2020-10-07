95c9109e7a07cd72979bbae0da69987253531aef
deanrivers <deanriversdeveloper@gmail.com>
2020-10-07 17:04:56 -0400

    Updated Signup Page look

--- frontend/src/PrivateRoute.js
+++ frontend/src/PrivateRoute.js
@@ -4,7 +4,7 @@ import { AuthContext } from "./Auth";
 
 const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
   const {currentUser} = useContext(AuthContext);
-  console.log('Current User ->',currentUser)
+  // console.log('Current User ->',currentUser)
   return (
     <Route
       {...rest}
